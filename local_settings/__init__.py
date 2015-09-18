wylee/django-local-settings

commit de95645baaaa1cdfeacc8290d297b22b5eab16d0

Return a new OrderedDict from Loader.load()
...and, consequently, from load_and_check_settings().

local_settings/__init__.py
@@ -13,12 +13,20 @@ from .__main__ import make_local_settings
 
 def load_and_check_settings(base_settings,  file_name=None, section=None, base_path=None,
                             quiet=NO_DEFAULT):
-    """Load settings from file into base settings, then check settings.
+    """Merge local settings from file with base settings, then check.
+
+    Returns a new OrderedDict containing the base settings and the
+    loaded settings. Ordering is:
+
+        - base settings
+        - settings from extended file(s), if any
+        - settings from file
+
+    When a setting is overridden, it gets moved to the end.
 
     Settings loaded from the specified file will override base settings,
     then the settings will be checked to ensure that all required local
-    settings have been set. Note that this modifies ``base_settings`` in
-    place.
+    settings have been set.
 
     If a file name is passed: if the file exists, local settings will be
     loaded from it and any missing settings will be appended to it; if
@@ -58,21 +66,21 @@ def load_and_check_settings(base_settings,  file_name=None, section=None, base_p
     try:
         try:
             loader = Loader(file_name, section)
-            loader.load(base_settings)
+            settings = loader.load(base_settings)
             registry = loader.registry
         except SettingsFileNotFoundError:
             registry = None
         checker = Checker(file_name, section, registry=registry)
-        success = checker.check(base_settings)
+        success = checker.check(settings)
     except KeyboardInterrupt:
         # Loading/checking of local settings was aborted with Ctrl-C.
         # This isn't an error, but we don't want to continue.
         if not quiet:
             printer.print_warning('\nAborted loading/checking of local settings')
         sys.exit(0)
-    if success:
-        if not quiet:
-            printer.print_success('Settings loaded successfully from {0}'.format(file_name))
-    else:
+    if not success:
         raise LocalSettingsError(
             'Could not load local settings from {0}'.format(file_name))
+    if not quiet:
+        printer.print_success('Settings loaded successfully from {0}'.format(file_name))
+    return settings

local_settings/loader.py
@@ -40,7 +40,18 @@ class Loader(Base):
         return settings
 
     def load(self, base_settings):
-        """Load local settings from file into base settings."""
+        """Merge local settings from file with ``base_settings``.
+
+        Returns a new OrderedDict containing the base settings and the
+        loaded settings. Ordering is:
+
+            - base settings
+            - settings from extended file(s), if any
+            - settings from file
+
+        When a setting is overridden, it gets moved to the end.
+
+        """
         if not os.path.exists(self.file_name):
             self.print_warning(
                 'Local settings file `{0}` not found'.format(self.file_name))
@@ -49,7 +60,7 @@ class Loader(Base):
         for k, v in self.read_file().items():
             names = k.split('.')
             v = self._parse_setting(v, expand_vars=True)
-            obj = base_settings
+            obj = settings
             for name, next_name in zip(names[:-1], names[1:]):
                 next_name = self._convert_name(next_name)
                 next_is_seq = isinstance(next_name, int)
@@ -75,7 +86,8 @@ class Loader(Base):
                     self.registry[curr_v] = name
             obj[name] = v
             settings.move_to_end(names[0])
-        self._do_interpolation(base_settings, base_settings)
+        self._do_interpolation(settings, settings)
+        return settings
 
     def _do_interpolation(self, v, settings):
         if isinstance(v, string_types):

commit a85b9dd17f3175493681665a44b644718742a253

When settings file isn't found, just check base settings
Instead of exploding because the local var `setting` isn't set. This
fixes a regression introduced in de95645.

local_settings/__init__.py
@@ -70,6 +70,7 @@ def load_and_check_settings(base_settings,  file_name=None, section=None, base_p
             registry = loader.registry
         except SettingsFileNotFoundError:
             registry = None
+            settings = base_settings
         checker = Checker(file_name, section, registry=registry)
         success = checker.check(settings)
     except KeyboardInterrupt: