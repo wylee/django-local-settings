0d43c8a8d41bbed4f5109b072e58d2f147d03669
Wyatt Baldwin <self@wyattbaldwin.com>
2019-11-21 15:43:14 -0800

    Remove duplicated Python 2.7 from trove classifiers

--- setup.py
+++ setup.py
@@ -67,7 +67,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
     ] + [
         'Programming Language :: Python :: {v}'.format(v=v)for v in supported_py_versions
