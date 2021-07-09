617045a744e62be12840a28d0b6d24f62915d960
Wyatt Baldwin <self@wyattbaldwin.com>
2021-07-09 04:02:13 -0700

    [jsonish] Handle JSONObject & datetime in encoder

--- src/jsonish/src/jsonish/encoder.py
+++ src/jsonish/src/jsonish/encoder.py
@@ -1,14 +1,31 @@
 """JSONish Encoder
 
-For now, this just passes through to the stdlib `json` module.
+This passes through to the stdlib `json` module with handling of
+:class:`JSONObject` and :class:`datetime.datetime` values.
 
 """
+import datetime
 import json
+import functools
+
+from .obj import JSONObject
 
 
 __all__ = ["encode", "encode_to_file", "Encoder"]
 
 
-encode = json.dumps
-encode_to_file = json.dump
-Encoder = json.JSONEncoder
+def default(obj):
+    if isinstance(obj, JSONObject):
+        return dict(obj)
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
+
+
+encode = functools.partial(json.dumps, default=default)
+encode_to_file = functools.partial(json.dump, default=default)
+
+
+class Encoder(json.JSONEncoder):
+    def default(self, obj):
+        return default(obj)
